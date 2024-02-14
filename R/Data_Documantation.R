2b81e8d523e6ff39151ed9e197835683a051d464
elahe-elahi <elaheh.2110@gmail.com>
2024-02-14 10:27:02 -0800

    Initial commit

---
+++ R/Data_Documantation.R
@@ -0,0 +1,42 @@
+#' Title for R99Data
+#'
+#' @format A data frame with 17 variables:
+#' \describe{
+#'   \item{Address}{Description of var1}
+#'   \item{MahMorajeh}{Description of var2}
+#'   \item{Fasl}{Description of var3}
+#'   \item{weight}{Description of var4}
+#'   \item{NoeKhn}{Description of var5}
+#'   \item{Takmil}{Description of var6}
+#'   \item{TakmilDescA}{Description of var7}
+#'   \item{TakmilDescB}{Description of var8}
+#'   \item{TakmilDescC}{Description of var9}
+#'   \item{Jaygozin}{Description of var10}
+#'   \item{JaygozinDescA}{Description of var11}
+#'   \item{JaygozinDescB}{Description of var12}
+#'   \item{JaygozinDescC}{Description of var13}
+#'   \item{BlkAbdJaygozin}{Description of var14}
+#'   \item{RadifJaygozin}{Description of var15}
+#'   \item{IsTel}{Description of var16}
+#'   \item{FVam}{Description of var17}
+#' }
+"R99Data"
+
+#' Title for R99P1
+#'
+#' @format A data frame with 10 variables:
+#' \describe{
+#'   \item{Address}{Description of var1}
+#'   \item{DYCOL01}{Description of var2}
+#'   \item{DYCOL03}{Description of var3}
+#'   \item{DYCOL04}{Description of var4}
+#'   \item{DYCOL05}{Description of var5}
+#'   \item{DYCOL06}{Description of var6}
+#'   \item{DYCOL07}{Description of var7}
+#'   \item{DYCOL08}{Description of var8}
+#'   \item{DYCOL09}{Description of var9}
+#'   \item{DYCOL10}{Description of var10}
+#' }
+"R99P1"
+
+
